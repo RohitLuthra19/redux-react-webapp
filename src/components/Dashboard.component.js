28f3a701d282a5f3b8cdb573823bdfcad034daba
rkumar <rokumar@quark.com>
2018-03-22 20:08:02 +0530

    Added dashboard ui

--- src/components/Dashboard.component.js
+++ src/components/Dashboard.component.js
@@ -1,8 +1,71 @@
 import React, { Component } from 'react';
+import { withStyles } from 'material-ui/styles';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import List from 'material-ui/List';
+import Typography from 'material-ui/Typography';
+import Divider from 'material-ui/Divider';
+import {listItems} from './MenuList.component';
 
-export default class DashboardComponent extends Component {
+const drawerWidth = 240;
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        height: '100vh',
+        zIndex: 1,
+        overflow: 'hidden',
+        position: 'relative',
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    drawerPaper: {
+        position: 'relative',
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing.unit * 3,
+        minWidth: 0, // So the Typography noWrap works
+    },
+    toolbar: theme.mixins.toolbar,
+});
+
+class DashboardComponent extends Component {
 
     render() {
-        return (<div>Dashboard</div>);
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar position="absolute" className={classes.appBar}>
+                    <Toolbar>
+                        <Typography variant="title" color="inherit" noWrap>
+                            Dashboard
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    variant="permanent"
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                >
+                    <div className={classes.toolbar} />
+                    <List>{listItems}</List>
+                    <Divider />
+                </Drawer>
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+                    <Typography noWrap>{'Welcom user'}</Typography>
+                </main>
+            </div>
+        );
     }
 }
+
+
+export default withStyles(styles)(DashboardComponent);
